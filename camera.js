a980a22b87f04855b16858b366f367cea8f6442e
Grebencea Adelina <137497084+AdelinaGre@users.noreply.github.com>
2024-10-03 09:03:51 +0300

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,20 @@
+var video = documeat.gctElmentByld("video");
+
+function on_cam_success(stream){
+video.srcObject = stream;}
+
+function on_cam_error(err){
+alert("error"+err.message);}
+
+var constraints= { audio:false, video: true};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(on_cam_success)
+.catch(on_cam_error);
+
+function capteaza(){
+var c=document.getElementById("canvas");
+c.width = video.width;
+c.height = video.height;
+var ctx =e.getContext("2d");
+ctx.drawImage(video, 0,0,640,480);}
